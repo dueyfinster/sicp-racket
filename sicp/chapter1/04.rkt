615781cbdbe30e42665cb689a75ba46d5122c02c
Neil Grogan <neil@grogan.org>
2022-05-31 14:54:06 +0100

    Finished ch01e4

---
+++ sicp/chapter1/04.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+(#%require rackunit)
+
+
+;; Describe the function below
+(define (a-plus-abs-b a b)
+((if (> b 0) + -) a b))
+;; if b is greater than 0 - we add a + b
+;; else we do a - b
+
+(check-equal? (a-plus-abs-b -2 6) 4)
+(check-equal? (a-plus-abs-b 2 6) 8)
